d48b2de2e67a51e0e23e6351b5fdee86b8d9eb83
LeonardoLopes21 <101759612+LeonardoLopes21@users.noreply.github.com>
2024-04-11 19:43:04 -0300

    Added Validation for more than 1 Name

--- scripts/validations.js
+++ scripts/validations.js
@@ -18,12 +18,26 @@ function containNumbers(id){
 function nameValidationBlur(id){
 
     let element = $("#" + id);
-    if(isEmpty(id)||containNumbers(id)){
+    if(isEmpty(id)||containNumbers(id)||!hasTwoFullValues(id)){
         element.addClass("red-error");
     } else {
         element.removeClass("red-error");
     }
     
+
+}
+
+function hasTwoFullValues(id){
+    let element = $("#" + id);
+    let array = element.val().split(" ")
+    let filteredArray = []
+    for(let i in array){
+        if(array[i] != "" && array[i].length > 1){
+            filteredArray.push(array[i])
+        }
+    }
+
+    return filteredArray.length > 1;
 
 }
 
@@ -36,23 +50,64 @@ function nameValidationHover(id){
         if(element.title.length < 1){
             element.setAttribute("title", element.title += "Campo está vazio!");
         } else {
-            element.setAttribute("title", "\nCampo está vazio!")
+            element.setAttribute("title", element.title += "\nCampo está vazio!")
         }
         errCount++
-    } else {
-        element.setAttribute("title", "")
     }
 
     if(containNumbers(id)){
         if(element.title.length < 1){
             element.setAttribute("title", element.title += "Campo contém números!");
         } else {
-            element.setAttribute("title", "\nCampo contém números!")
+            element.setAttribute("title", element.title += "\nCampo contém números!")
         }
         errCount++
     }
 
-    if(errCount > 1){
-        element.setAttribute("title", "Field is Correct! :D");
+    if(!hasTwoFullValues(id)){
+        if(element.title.length < 1){
+            element.setAttribute("title", element.title += "Nome não está completo!");
+        } else {
+            element.setAttribute("title", element.title += "\nNome não está completo!")
+        }
+        errCount++
     }
+
+    if(errCount < 1){
+        element.setAttribute("title", ":D");
+    }
+}
+
+function eliminateUnwantedChars(unwantedchars, text){
+    let newText = text;
+    newText.replace("1", "")
+    for(let i in unwantedchars){
+        if(newText.includes(unwantedchars[i])){
+            newText = newText.replace(unwantedchars[i], "");
+        }
+    }
+
+    return newText;
+}
+
+function cpfAdjusting(){
+
+    let input = $("#input-cpf")
+    let inputVal = input.val();
+
+    let invalidChars = "abcdefghijlmnopqrstuvwxyzáéíóúãõ;!@#$%¨&*()_+="
+    let upperCaseInvalidChars = invalidChars.toUpperCase();
+
+    inputVal = eliminateUnwantedChars(invalidChars, inputVal);
+    inputVal = eliminateUnwantedChars(upperCaseInvalidChars, inputVal);
+
+    input.val(inputVal);
+
+    input.val();
+    if(inputVal.length == 3 || inputVal.length == 7){
+        input.val(inputVal + ".")
+    } else if(inputVal.length == 11){
+        input.val(inputVal + "-");
+    }
+
 }
